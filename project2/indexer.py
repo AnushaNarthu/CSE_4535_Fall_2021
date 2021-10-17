5c11ec5668668dc26b43dc84741b9cb45c5f959b
Anusha Narthu <78515787+AnushaNarthu@users.noreply.github.com>
2021-10-17 01:29:36 -0400

    Update indexer.py

--- project2/indexer.py
+++ project2/indexer.py
@@ -24,11 +24,23 @@ class Indexer:
             self.add_to_index(t, doc_id)
 
     def add_to_index(self, term_, doc_id_):
+        if term_ in self.inverted_index.keys():
+            posting_list = self.inverted_index[term_]
+            list1 = posting_list.traverse_list()
+            if doc_id_ not in list1:
+                posting_list.insert_at_end(doc_id_)
+                self.inverted_index[term_] = posting_list
+        else:
+            posting_list = LinkedList()
+            posting_list.insert_at_end(doc_id_)
+            self.inverted_index[term_] = posting_list
+
         """ This function adds each term & document id to the index.
             If a term is not present in the index, then add the term to the index & initialize a new postings list (linked list).
             If a term is present, then add the document to the appropriate position in the posstings list of the term.
             To be implemented."""
-        raise NotImplementedError
+
+        
 
     def sort_terms(self):
         """ Sorting the index by terms.
@@ -39,11 +51,30 @@ class Indexer:
         self.inverted_index = sorted_index
 
     def add_skip_connections(self):
+        for value in self.inverted_index.keys():
+            pointer = self.inverted_index[value]
+            pointer.add_skip_connections()
+
         """ For each postings list in the index, add skip pointers.
             To be implemented."""
-        raise NotImplementedError
+            
+        return
 
-    def calculate_tf_idf(self):
-        """ Calculate tf-idf score for each document in the postings lists of the index.
-            To be implemented."""
-        raise NotImplementedError
+    def calculate_tf_idf(self,docs_count,count_term):
+        for value in self.inverted_index.values():
+            m = value.start_node
+            while m is not None:
+                doc_freq = value.length
+                term_freq = m.count_terms/count_term[m.value]
+                docs_count = docs_count
+                idf = docs_count/doc_freq
+                m.tf_idf = term_freq*idf
+                print(m.tf_idf)
+                m = m.next
+                """ Calculate tf-idf score for each document in the postings lists of the index.
+              To be implemented."""
+        return
+        
+        
+    
+      
